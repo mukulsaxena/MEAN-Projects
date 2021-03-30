5e28d14f9f6ed569614ea0b12395793580242191
mukulsaxena <mukulchandra@hotmail.com>
2021-03-30 06:28:18 -0400

    Update app.js

--- Project5/app.js
+++ Project5/app.js
@@ -1,21 +1,20 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
-
-var mongoose = require('mongoose')
-var dev_db_url = "mongodb+srv://mukul:mukul123@mukul.xwysp.mongodb.net/mean-projects"
+require('./database/initDB')();
+//var mongoose = require('mongoose')
 
 var routes = require('./router/todo')
 
-mongoose.connect(dev_db_url)
-mongoose.Promise = global.Promise
-var db = mongoose.connection
+//mongoose.connect(dev_db_url)
+//mongoose.Promise = global.Promise
+//var db = mongoose.connection
 
 app.get('/', (req,res) => {
     res.send("My Todo API is Running.")
 })
 
-db.on('error',console.error.bind(console,'MongoDB Connection Error'));
+//db.on('error',console.error.bind(console,'MongoDB Connection Error'));
 
 
 
